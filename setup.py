c81eeefe9e7c958029202c388c463b22be9b3d8d
Ben Rowan <ben.rob.rowan@gmail.com>
2022-03-30 14:25:00 +0100

    Upgrade feedparser to version 6.0.8

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(name='podfox',
     install_requires=[
         'colorama==0.3.7',
         'docopt==0.6.2',
-        'feedparser==5.2.1',
+        'feedparser==6.0.8',
         'requests==2.20.0',
         ],
     )
